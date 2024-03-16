1bdb9576264ca5c60866183e1c83354e5188a6a9
Monir Ullah <mullah725@gmail.com>
2024-03-17 00:09:55 +0600

    try catch added

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,8 +2,20 @@ import { Request, Response } from "express";
 import { UserServices } from "./user.service";
 
 const createAmin = async (req: Request, res: Response) => {
-  const result = await UserServices.createAdminIntoDB(req.body);
-  res.send(result);
+  try {
+    const result = await UserServices.createAdminIntoDB(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin Created Successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something Went wrong",
+      data: error,
+    });
+  }
 };
 
 export const UserController = {
